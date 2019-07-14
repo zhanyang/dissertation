d45fce605c9fad86a1db82847d5030762652f9bc
Yang Zhan <yzhan@cs.unc.edu>
2019-07-14 19:50:59 -0400

    xfs toku

---
+++ data/toku.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+in_dir=raw/toku
+out_dir=toku
+
+for fs in ext4 btrfs xfs
+do
+    in_file=${in_dir}/${fs}.csv
+    out_file=${out_dir}/${fs}.csv
+    truncate -s 0 ${out_file}
+    while read line; do
+        c1=`cut -f1 -d, <<< ${line}`;
+        c3=`cut -f3 -d, <<< ${line}`;
+        echo ${c1} ${c3} >> ${out_file};
+    done < ${in_file}
+done
